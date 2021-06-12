2b035e97407c94bf25acad38b1107fd888098e3d
JL-08 <janellelacsamana@gmail.com>
2021-06-12 16:59:47 +0800

    add posts content

--- src/components/Home/Posts.js
+++ src/components/Home/Posts.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
+import {Card, List, Text} from '@ui-kitten/components';
+
+const data = new Array(8).fill({
+  title: 'Item',
+});
 
 const Posts = () => {
+  const renderItemHeader = (headerProps, info) => (
+    <View {...headerProps}>
+      <Image
+        style={styles.img}
+        source={require('../../images/post-sample-img.png')}
+      />
+    </View>
+  );
+
+  const renderItemFooter = footerProps => (
+    <Text {...footerProps}>By Wikipedia</Text>
+  );
+
+  const renderItem = info => (
+    <Card
+      style={styles.item}
+      status="basic"
+      header={headerProps => renderItemHeader(headerProps, info)}
+      footer={renderItemFooter}>
+      <Text>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Text>
+    </Card>
+  );
+
   return (
-    <View>
-      <Text>Posts</Text>
+    <View style={styles.container}>
+      <List
+        contentContainerStyle={styles.contentContainer}
+        data={data}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  contentContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  img: {
+    alignSelf: 'center',
+  },
+});
 
 export default Posts;
