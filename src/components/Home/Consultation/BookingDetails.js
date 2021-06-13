6e8b5cc926cb365be9338f91ffbce8ad0bbc7d14
JL-08 <janellelacsamana@gmail.com>
2021-06-13 12:15:20 +0800

    add booking details

--- src/components/Home/Consultation/BookingDetails.js
+++ src/components/Home/Consultation/BookingDetails.js
@@ -1,14 +1,14 @@
+import {Text, Button, Divider} from '@ui-kitten/components';
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const BookingDetails = () => {
+const BookingDetails = ({navigation: {goBack}}) => {
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity
         style={{
-          marginTop: 10,
-          marginLeft: 20,
+          marginVertical: 10,
           display: 'flex',
           flexDirection: 'row',
         }}
@@ -16,9 +16,71 @@ const BookingDetails = () => {
         <Icon name="chevron-left" size={23} color="#999" />
         <Text style={{marginLeft: 5, color: '#444'}}>Back</Text>
       </TouchableOpacity>
-      <Text>Book</Text>
+      <Text style={styles.title} category="h5">
+        BOOKING FEE
+      </Text>
+      <View style={styles.tableContainer}>
+        <View style={styles.description}>
+          <Text category="h6" style={styles.textPadding}>
+            Consultation Fee
+          </Text>
+          <Text category="h6" style={styles.textPadding}>
+            Service Charge
+          </Text>
+          <Text category="h6" style={styles.textBorder}>
+            Total
+          </Text>
+        </View>
+        <View style={styles.amounts}>
+          <Text category="h6" style={styles.textPadding}>
+            300
+          </Text>
+          <Text category="h6" style={styles.textPadding}>
+            70
+          </Text>
+          <Text category="h6" style={styles.textBorder}>
+            370
+          </Text>
+        </View>
+      </View>
+      <Text category="p2" style={{color: '#555'}}>
+        NOTE: Booking fee will be completely refunded if the veterinarian
+        cancels or does not accept your appointments after 24hours of booking.
+      </Text>
+      <Button>BOOK APPOINTMENT</Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+  },
+  container: {
+    padding: 15,
+  },
+  tableContainer: {
+    borderWidth: 1,
+    borderRadius: 20,
+    flexDirection: 'row',
+    padding: 15,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  description: {
+    flex: 1,
+  },
+  amounts: {
+    textAlign: 'right',
+  },
+  textBorder: {
+    borderTopWidth: 2,
+    paddingVertical: 3,
+  },
+  textPadding: {
+    paddingVertical: 3,
+  },
+});
 
 export default BookingDetails;
