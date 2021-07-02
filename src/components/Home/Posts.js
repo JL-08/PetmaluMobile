754eeb9c65015de1dc9668eb21d1593feb738175
JL-08 <janellelacsamana@gmail.com>
2021-07-03 01:32:19 +0800

    add get posts

--- src/components/Home/Posts.js
+++ src/components/Home/Posts.js
@@ -1,49 +1,68 @@
-import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
-import {Card, List, Text, Divider} from '@ui-kitten/components';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {View, StyleSheet, Image, RefreshControl} from 'react-native';
+import {Card, List, Text, Divider, Modal} from '@ui-kitten/components';
 import ContentTitle from './ContentTitle';
+import moment from 'moment';
+
+import {getAllPosts} from '../../actions/postActions';
 
 const data = new Array(8).fill({
   title: 'Item',
 });
 
 const Posts = () => {
-  const renderItemHeader = (headerProps, info) => (
+  const [refreshing, setRefreshing] = React.useState(false);
+  const posts = useSelector(state => state.post.postData);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setRefreshing(true);
+    dispatch(getAllPosts(setRefreshing));
+  }, []);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    dispatch(getAllPosts(setRefreshing));
+  }, []);
+
+  const renderItemHeader = (headerProps, item) => (
     <View {...headerProps}>
       <View style={styles.row}>
         <Text style={{fontWeight: 'bold', flex: 1}} category="h6">
-          Title 1
+          {item.title}
         </Text>
         <Text style={{color: '#888', alignSelf: 'flex-end'}} category="c1">
-          06/20/2021 9:00PM
+          {moment(item.created_at).format('MMM DD, YYYY hh:mm A')}
         </Text>
       </View>
-      <Divider style={{marginVertical: 10}} />
-      <Image
-        style={styles.img}
-        source={require('../../images/post-sample-img.png')}
-      />
+      {item.img_name && (
+        <>
+          <Divider style={{marginVertical: 10}} />
+          <Image
+            style={styles.img}
+            source={{
+              uri: `http://10.0.2.2/petsmalu/upload/images/${item.img_name}`,
+            }}
+          />
+        </>
+      )}
     </View>
   );
 
-  const renderItemFooter = footerProps => (
-    <Text {...footerProps}>By Wikipedia</Text>
+  const renderItemFooter = (footerProps, item) => (
+    <View style={{flex: 1}}>
+      <Text {...footerProps}>By: {item.vet_name}</Text>
+    </View>
   );
 
-  const renderItem = info => (
+  const renderItem = ({item}) => (
     <Card
       style={styles.item}
       status="basic"
-      header={headerProps => renderItemHeader(headerProps, info)}
-      footer={renderItemFooter}>
-      <Text>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </Text>
+      header={headerProps => renderItemHeader(headerProps, item)}
+      footer={footerProps => renderItemFooter(footerProps, item)}>
+      <Text>{item.body}</Text>
     </Card>
   );
 
@@ -52,8 +71,11 @@ const Posts = () => {
       <ContentTitle title="Latest Posts" />
       <List
         contentContainerStyle={styles.contentContainer}
-        data={data}
+        data={posts}
         renderItem={renderItem}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </View>
   );
@@ -62,6 +84,9 @@ const Posts = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 15,
+    position: 'relative',
+    paddingBottom: 50,
+    paddingTop: 0,
   },
   contentContainer: {
     paddingHorizontal: 8,
@@ -69,6 +94,37 @@ const styles = StyleSheet.create({
   },
   img: {
     alignSelf: 'center',
+    width: 400,
+    height: 300,
+  },
+  floatBtn: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    position: 'absolute',
+    top: '13%',
+    right: '10%',
+    zIndex: 10,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  modal: {
+    minHeight: 200,
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderWidth: 5,
+  },
+  modalText: {
+    height: 150,
+    justifyContent: 'center',
+  },
+  btnContainer: {
+    justifyContent: 'space-around',
+  },
+  contentTitle: {
+    fontWeight: 'bold',
+    flex: 1,
   },
 });
 
